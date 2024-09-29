84182262644b3290b86b3582204d74b626385b0f
Kishlay Kumar <68921894+kkishlayofficial@users.noreply.github.com>
2024-09-29 23:51:17 +0530

    Fixed issue

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,4 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDK9I-5svOasNLZU3WcusBqrex0pJCpiTE",
+  authDomain: "netflixgpt-9ce4c.firebaseapp.com",
+  projectId: "netflixgpt-9ce4c",
+  storageBucket: "netflixgpt-9ce4c.appspot.com",
+  messagingSenderId: "1024945958087",
+  appId: "1:1024945958087:web:f47327bec4737fd86fe2e2",
+  measurementId: "G-WRK058THJK",
+};
+
 // Initialize Firebase
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const provider = new GoogleAuthProvider();
